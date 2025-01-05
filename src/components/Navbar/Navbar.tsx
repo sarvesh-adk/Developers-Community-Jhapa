296b424383a5d8339090e6a8564f8435cbbdd18b
Sarvesh-adk <sarbeswar.adk@gmail.com>
2025-01-05 18:15:55 +0530

    update1

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <span className="text-2xl font-bold text-[#022623] dark:text-white">|| वसुधैव कुटुम्बकम् ||</span>
+            <span className="text-2xl font-normal text-[#022623] dark:text-white">|| नमस्ते ||</span>
           </div>
 
           {/* Desktop Navigation */}
--- src/components/sections/Speakers.tsx
+++ src/components/sections/Speakers.tsx
@@ -14,7 +14,7 @@ const speakers = [
     image: 'https://scontent.fixc4-1.fna.fbcdn.net/v/t39.30808-6/318311092_2157036501134772_6190539364176794189_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=LZOLkf20XZQQ7kNvgHLlUqi&_nc_zt=23&_nc_ht=scontent.fixc4-1.fna&_nc_gid=A7kgtmiuC7LufmmxiNCxQk4&oh=00_AYBWVUQyMTEc7HML_z4R1ZZUD077ZffDd055Swg7cyDZCA&oe=677FFFAD'
   },
   {
-    name: 'Emily Brown',
+    name: 'Arjun Tiwari',
     role: 'CEO at Astral TechSoft | Professor',
     image: 'https://scontent.fixc4-3.fna.fbcdn.net/v/t39.30808-6/393743354_6735167199910986_792954933038599466_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=R03SusRuAEcQ7kNvgG7z37z&_nc_zt=23&_nc_ht=scontent.fixc4-3.fna&_nc_gid=Ah-LfBTiKkqJhjuA3X9i28E&oh=00_AYAFj-udMApXvVBIZ4xTTkXp089GEd1hXjxiN7qo0511Nw&oe=67800996'
   }
