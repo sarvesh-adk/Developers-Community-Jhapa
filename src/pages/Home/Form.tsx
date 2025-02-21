e6b5a5137e6e7095ace5192d334f135b85b15bbb
Sarvesh Kashyap <sarbeswar.adk@gmail.com>
2025-02-21 23:27:05 +0545

    Form

---
+++ src/pages/Home/Form.tsx
@@ -0,0 +1,18 @@
+const Form = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <div></div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfQRx8kwwDtocvOfDUhQVqUZl9MrytJkyNJLHL2qG5D8dFhjg/viewform?embedded=true"
+          width="600"
+          height="600"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </>
+  );
+};
+
+export default Form;
